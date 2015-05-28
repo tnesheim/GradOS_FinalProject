3faf7c7cb6c69e18868ecd9bc19129c1eaa24da5
tnesheim <tnesheim@calpoly.edu>
2015-05-28 12:14:41 -0700

    Working on getting RIOT OS to compile.

--- with_riot/master_node/examples/hello-world/bin/native/cpu/tramp.d
+++
@@ -1,2 +0,0 @@
-/Users/tnesheim/GitHub/GradOS_FinalProject/with_riot/master_node/examples/hello-world/bin/native/cpu/tramp.o: \
-  /Users/tnesheim/GitHub/GradOS_FinalProject/with_riot/master_node/cpu/native/tramp.S
--- with_riot/master_node/examples/hello-world/bin/pca10005/cpu/lpm_arch.d
+++
@@ -1,5 +0,0 @@
-/Users/tnesheim/GitHub/GradOS_FinalProject/with_riot/master_node/examples/hello-world/bin/pca10005/cpu/lpm_arch.o: \
- /Users/tnesheim/GitHub/GradOS_FinalProject/with_riot/master_node/cpu/nrf51822/lpm_arch.c \
- /Users/tnesheim/GitHub/GradOS_FinalProject/with_riot/master_node/core/include/arch/lpm_arch.h
-
-/Users/tnesheim/GitHub/GradOS_FinalProject/with_riot/master_node/core/include/arch/lpm_arch.h:
